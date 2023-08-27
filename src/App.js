71ae97dd56074bb90ace9952ae4f65ef8ac5bbab
anmoln <sirkinggreat@gmail.com>
2023-08-27 15:08:33 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header/Header.js';
+import Upload from './components/Upload/Upload.js';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Upload />
+    </div>
+  );
+}
+
+export default App;
