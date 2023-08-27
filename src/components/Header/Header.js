78bf95ecb0743d22bd5f62abf16898fcf9dfab8b
anmoln <sirkinggreat@gmail.com>
2023-08-27 19:11:10 +0530

    s

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,10 @@
 import React from 'react';
-
+const x=0
 const Header = () => {
+  if(x==0){
+    window.alert("Kindly wait 30-40 seconds after clicking upload for backend to load.Thanks for the patience")
+    x=1
+  }
   const headerStyle = {
     background: '#f0f0f0', // Greyish shade background
     color: '#333', // Dark text color
